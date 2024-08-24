551ce8e9f2ca34be72f615e9b5c7252aae795feb
Sergey Gasparyan <gasparyan.sego.07@gmail.com>
2024-08-24 18:05:11 +0400

    I write representing persion

---
+++ representingPersion.js
@@ -0,0 +1,41 @@
+// TASK 3
+// 3.1
+// const obj = {
+//     firstName : "Sergey",
+//     lastName : "Gasparyan",
+//     age : 17
+// } 
+
+// obj.toString = function() {
+//     return ` Name ${obj.firstName} ${obj.lastName} , Age : ${obj.age}`
+// }
+
+
+
+// 3.2
+// const obj = {
+//     price: 2500,
+// }
+// obj.valueOf = function() {
+//     return obj.price
+// }
+// console.log(obj + 1500)
+
+
+
+// 3.3
+// const data = {
+//     day: 18,
+//     month : 8 ,
+//     year : 2024
+// }
+// data.toString = function () {
+//     let day = data.day > 10 ? data.day : `0${data.day}`
+//     let month = data.month > 10 ? data.month : `0${data.month}`
+//     return `${day} / ${month} / ${data.year}`
+// }
+// data.valueOf = function () {
+//     return Date(100000)
+// }
+// console.log(+(data))
+
