4ba72223464cccd193b8a113afb46e575cf1a3d8
Sergey Gasparyan <gasparyan.sego.07@gmail.com>
2024-08-24 18:03:07 +0400

    I write what will print in my opinion

---
+++ whatWillPrint.js
@@ -0,0 +1,55 @@
+
+// . Number('0x77') === 0x77 // false kta NaN
+// . Number('077') === 077 // false
+// . Number(' 12') // 12
+// . isNaN(' ') // false
+// . +'' // 0
+// . .1 + .2 == .3 // false
+// . 9007199254740992 + 1 == 9007199254740992 // true
+// . 9007199254740992 + 2 == 9007199254740992 // false
+// . 0 === +0 // true
+// . +0 === -0 // true
+// . 1 / '2' // 1 / 2
+// . 1 / 0 // infinity
+// . 1 / -0 // -infinity
+// . Infinity / 6 // infinity
+// . Infinity / -256 // - infinity
+// . Infinity / Infinity // anoroshutyun e 
+// . 0 / 0 // NaN anoroshutyun e
+// . +true // 1
+// . +false //0
+// . +null // 0
+// . +undefined // NaN
+// . parseInt('.2') // NaN ete tvov chi sksum NaN a tali
+// . parseInt('077a') // 77  
+// . parseInt('0x77a') // 1914 
+// . parseInt('I’m the best value - said Infinity.') // NaN
+// . parseFloat('I’m the best value - said Infinity.') // NaN
+// . parseInt('Infinity is the best value!') //NaN 
+// . parseFloat('Infinity is the best value!') // NaN
+// . parseFloat('12.78ff') // 12.78    simvolnery antesuma
+
+
+
+
+
+// TASK 2
+// . "" + 1 + 0 // "1" + 0 = "10"
+// . "" - 1 + 0 //0 -1 + 0 = -1
+// . true + false // 1 + 0 = 1
+// . 6 / "3" // 2
+// . "2" * "3" // 6
+// . 4 + 5 + "px" // "9px"
+// . "$" + 4 + 5 // "$45"
+// "4" - 2 // 2
+// . "4px" - 2 // NaN
+// . 7 / 0 //Infinity
+// . " -9 " + 5 // -4
+// . " -9 " - 5 // -14
+// . null + 1 //1
+// . undefined + 1 // NaN
+
+
+
+
+
